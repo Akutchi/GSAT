42386a55f2bad9dc4adbbf97708a0bdae63b3f5b
Akutchi <kaitlyn.buffler@tutamail.com>
2025-02-22 16:13:50 +0100

    parse and print package body

--- src/AST/expressions.adb
+++ src/AST/expressions.adb
@@ -2,9 +2,9 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 package body Expressions is
 
-   -----------
-   -- Parse --
-   -----------
+   -------------------------
+   -- Get_Until_Semicolon --
+   -------------------------
 
    procedure Get_Until_Semicolon (Str : in out SU.Unbounded_String;
                                   Backbone : in out T_Buffer.AST_Backbone)
@@ -23,6 +23,10 @@ package body Expressions is
       end loop;
 
    end Get_Until_Semicolon;
+
+   ----------------------
+   -- Parse_Dependency --
+   ----------------------
 
    procedure Parse_Dependency (Exp : in out Expression'Class;
                               Backbone : in out T_Buffer.AST_Backbone)
@@ -48,12 +52,50 @@ package body Expressions is
 
    end Parse_Dependency;
 
+   procedure Parse_Package (V : in out Visitor;
+                             Exp : in out Expression'Class;
+                             Backbone : in out T_Buffer.AST_Backbone)
+   is
+      Name           : SU.Unbounded_String;
+      Has_Body       : Boolean := False;
+      Declarations   : constant Expr_List.Vector := Expr_List.Empty_Vector;
+      Body_Expr      : constant Expr_List.Vector := Expr_List.Empty_Vector;
+
+      Current_Token : T_Buffer.Char_Buffer;
+
+   begin
+
+      Current_Token := Backbone.Next;
+
+      if Current_Token.Kind = Constants.identifier_t then
+
+         Name := SU.To_Unbounded_String (Current_Token.Buffer_To_String);
+
+      elsif Current_Token.Kind = Constants.body_t then
+
+         Has_Body := True;
+         Name := SU.To_Unbounded_String (Backbone.Next.Buffer_To_String);
+      end if;
+
+      Exp := Expression'Class (Container_Expr (Exp).Make (Constants.package_t,
+                                                          Name,
+                                                          Has_Body,
+                                                          Declarations,
+                                                          Body_Expr));
+
+   end Parse_Package;
+
+   ----------------
+   -- Parse_File --
+   ----------------
+
    procedure Parse_File (V : in out Visitor;
                          Exp : in out Expression'Class;
                          Backbone : in out T_Buffer.AST_Backbone)
    is
       F_Name         : SU.Unbounded_String;
       Dependencies   : Expr_List.Vector := Expr_List.Empty_Vector;
+      Container      : Container_Expr;
 
       Current_Token : T_Buffer.Char_Buffer;
 
@@ -76,10 +118,17 @@ package body Expressions is
          end;
       end loop;
 
-      Exp := Expression'Class
-         (File_Expr (Exp).Make (Dependencies, F_Name));
+      Container.Parse (V, Backbone);
+
+      Exp := Expression'Class (File_Expr (Exp).Make (F_Name,
+                                                     Dependencies,
+                                                     Container));
 
    end Parse_File;
+
+   -----------
+   -- Parse --
+   -----------
 
    procedure Parse (V : in out Visitor; Exp : in out Expression'Class;
                     Backbone : in out T_Buffer.AST_Backbone)
@@ -90,8 +139,9 @@ package body Expressions is
 
       case Current_Token.Kind is
 
-         when Constants.file_t => Parse_File (V, Exp, Backbone);
-         when Constants.with_t => Parse_Dependency (Exp, Backbone);
+         when Constants.file_t    => Parse_File (V, Exp, Backbone);
+         when Constants.with_t    => Parse_Dependency (Exp, Backbone);
+         when Constants.package_t => Parse_Package (V, Exp, Backbone);
 
          when others => null;
 
@@ -105,9 +155,9 @@ package body Expressions is
       V.Parse (Exp, Backbone);
    end Parse;
 
-   -----------
-   -- Print --
-   -----------
+   ----------------
+   -- Print_With --
+   ----------------
 
    procedure Print_With (Exp : Dependency_Expr)
    is
@@ -125,34 +175,65 @@ package body Expressions is
 
    end Print_With;
 
+   procedure Print_Package (Exp : Container_Expr)
+   is
+   begin
+
+      Put ("package ");
+
+      if Exp.Has_Body then
+         Put ("body ");
+      end if;
+
+      Put_Line (SU.To_String (Exp.Name));
+      Put_Line ("is");
+      Put_Line ("begin");
+
+      Put_Line ("end " & SU.To_String (Exp.Name) & ";");
+
+   end Print_Package;
+
+   ----------------
+   -- Print_File --
+   ----------------
+
    procedure Print_File (Exp : File_Expr; V : in out Visitor)
    is
    begin
-      Put_Line (SU.To_String (Exp.Name));
+      Put_Line (SU.To_String (Exp.File_Name));
 
       for Dependency of Exp.Dependencies loop
          Dependency.Print (V);
       end loop;
 
-      Put_Line ("");
+      Put_Line (" ");
+      Exp.Container.Print (V);
+      Put_Line (" ");
 
    end Print_File;
+
+   -----------
+   -- Print --
+   -----------
 
    procedure Print (V : in out Visitor; Exp : Expression'Class)
    is
    begin
 
-      if Exp.Kind_T = Constants.file_t then
+      case Exp.Kind_T is
 
-         Print_File (File_Expr (Exp), V);
+         when Constants.file_t      => Print_File (File_Expr (Exp), V);
+         when Constants.with_t      => Print_With (Dependency_Expr (Exp));
+         when Constants.package_t   => Print_Package (Container_Expr (Exp));
 
-      end if;
+         when others => Put_Line ("Could not print " &
+                                  Constants.Lex_Type'Image (Exp.Kind_T));
 
-      if Exp.Kind_T = Constants.with_t then
+      end case;
 
-         Print_With (Dependency_Expr (Exp));
+   exception
 
-      end if;
+      when Constraint_Error => Put_Line ("Rest of the file");
 
    end Print;
 
@@ -165,6 +246,34 @@ package body Expressions is
    ----------
    -- Make --
    ----------
+
+   function Make
+      (D                   : Dependency_Expr;
+       Kind_T              : Constants.Lex_Type;
+       With_Str, Use_Str   : SU.Unbounded_String := SU.Null_Unbounded_String)
+   return Dependency_Expr
+   is
+   begin
+
+      return (Kind_T, With_Str, Use_Str);
+
+   end Make;
+
+   function Make
+      (C                   : Container_Expr;
+       Kind_T              : Constants.Lex_Type;
+       Name                : SU.Unbounded_String;
+       Has_Body            : Boolean;
+       Declarations        : Expr_List.Vector;
+       Body_Expr           : Expr_List.Vector)
+   return Container_Expr
+   is
+   begin
+
+      return (Kind_T => Kind_T, Name => Name, Has_Body => Has_Body,
+              Declarations => Declarations, Body_Expr => Body_Expr);
+
+   end Make;
 
    function Make (F              : File_Expr;
                   Kind_T         : Constants.Lex_Type)
@@ -180,30 +289,20 @@ package body Expressions is
    end Make;
 
    function Make (F              : File_Expr;
+                  F_Name         : SU.Unbounded_String;
                   Dependencies   : Expr_List.Vector;
-                  Name           : SU.Unbounded_String)
+                  Container      : Container_Expr'Class)
    return File_Expr
    is
       F_New : File_Expr;
    begin
 
       F_New.Kind_T := F.Kind_T;
+      F_New.File_Name   := F_Name;
       F_New.Dependencies := Dependencies;
-      F_New.Name   := Name;
+      F_New.Container := Container_Expr (Container);
 
       return F_New;
-
-   end Make;
-
-   function Make
-      (D                   : Dependency_Expr;
-       Kind_T              : Constants.Lex_Type;
-       With_Str, Use_Str   : SU.Unbounded_String := SU.Null_Unbounded_String)
-   return Dependency_Expr
-   is
-   begin
-
-      return (Kind_T, With_Str, Use_Str);
 
    end Make;
 
--- src/AST/expressions.ads
+++ src/AST/expressions.ads
@@ -32,22 +32,34 @@ package Expressions is
 
    type Dependency_Expr is new Expression with private;
 
+   type Container_Expr is new Expression with private;
+
    type File_Expr is new Expression with private;
-
-   function Make (F              : File_Expr;
-                  Kind_T         : Constants.Lex_Type)
-   return File_Expr;
-
-   function Make (F              : File_Expr;
-                  Dependencies   : Expr_List.Vector;
-                  Name           : SU.Unbounded_String)
-   return File_Expr;
 
    function Make
       (D                   : Dependency_Expr;
        Kind_T              : Constants.Lex_Type;
        With_Str, Use_Str   : SU.Unbounded_String := SU.Null_Unbounded_String)
    return Dependency_Expr;
+
+   function Make
+      (C                   : Container_Expr;
+       Kind_T              : Constants.Lex_Type;
+       Name                : SU.Unbounded_String;
+       Has_Body            : Boolean;
+       Declarations        : Expr_List.Vector;
+       Body_Expr           : Expr_List.Vector)
+   return Container_Expr;
+
+   function Make (F              : File_Expr;
+                  Kind_T         : Constants.Lex_Type)
+   return File_Expr;
+
+   function Make (F              : File_Expr;
+                  F_Name         : SU.Unbounded_String;
+                  Dependencies   : Expr_List.Vector;
+                  Container      : Container_Expr'Class)
+   return File_Expr;
 
 private
 
@@ -58,12 +70,20 @@ private
 
    end record;
 
-   type File_Expr is new Expression with record
+   type Container_Expr is new Expression with record
 
-      Dependencies   : Expr_List.Vector;
       Name           : SU.Unbounded_String;
+      Has_Body       : Boolean;
       Declarations   : Expr_List.Vector;
       Body_Expr      : Expr_List.Vector;
+
+   end record;
+
+   type File_Expr is new Expression with record
+
+      File_Name      : SU.Unbounded_String;
+      Dependencies   : Expr_List.Vector;
+      Container      : Container_Expr;
 
    end record;
 
